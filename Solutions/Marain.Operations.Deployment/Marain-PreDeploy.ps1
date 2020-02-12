3225a081166387912e9b4ff25261a843eb1aa1fe
Ian Griffiths <Ian.Griffiths@endjin.com>
2020-02-12 17:30:15 +0000

    Add support for new marain deployment model

---
+++ Solutions/Marain.Operations.Deployment/Marain-PreDeploy.ps1
@@ -0,0 +1,29 @@
+﻿<#
+This is called during Marain.Instance infrastructure deployment prior to the Marain-ArmDeploy.ps
+script. It is our opportunity to perform initialization that needs to complete before any Azure
+resources are created.
+
+We create the Azure AD Application that the Operations Control function will use to authenticate incoming
+requests. (Currently, this application is used with Azure Easy Auth, but the service could also
+use it directly.)
+
+#>
+
+# Marain.Instance expects us to define just this one function.
+Function MarainDeployment([MarainServiceDeploymentContext] $ServiceDeploymentContext) {
+
+    # Don't think we need an app for status, since it is open by design
+    #$StatusApp = $ServiceDeploymentContext.DefineAzureAdAppForAppService("status", "OperationsStatusAppId")
+
+    $ControlApp = $ServiceDeploymentContext.DefineAzureAdAppForAppService(
+        "control",
+         "OperationsControlAppId")
+
+    $ControllerAppRoleId = "77d9c620-a258-4f0b-945c-a7128e82f3ec"
+    $ControlApp.EnsureAppRolesContain(
+        $ControllerAppRoleId,
+        "Operations controller",
+        "Able to create and modify operations",
+        "OperationsController",
+        ("User", "Application"))
+}
