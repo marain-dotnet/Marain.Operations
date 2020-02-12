a1293714fad68ac6e430db293ad29ff12007f4ab
Ian Griffiths <Ian.Griffiths@endjin.com>
2020-02-12 17:30:15 +0000

    Add support for new marain deployment model

---
+++ Solutions/Marain.Operations.Deployment/Marain-ArmDeploy.ps1
@@ -0,0 +1,24 @@
+﻿<#
+This is called during Marain.Instance infrastructure deployment after the Marain-PreDeploy.ps
+script. It is our opportunity to create Azure resources.
+#>
+
+# Marain.Instance expects us to define just this one function.
+Function MarainDeployment([MarainServiceDeploymentContext] $ServiceDeploymentContext) {
+
+    $TemplateParameters = @{
+        appName="operations"
+        controlFunctionEasyAuthAadClientId=$ServiceDeploymentContext.Variables["OperationsControlAppId"]
+        appInsightsInstrumentationKey=$ServiceDeploymentContext.InstanceContext.ApplicationInsightsInstrumentationKey
+    }
+    $InstanceResourceGroupName = $InstanceDeploymentContext.MakeResourceGroupName("operations")
+    $DeploymentResult = $ServiceDeploymentContext.InstanceContext.DeployArmTemplate(
+        $PSScriptRoot,
+        "deploy.json",
+        $TemplateParameters,
+        $InstanceResourceGroupName)
+
+    $ServiceDeploymentContext.Variables["KeyVaultName"] = $DeploymentResult.Outputs.keyVaultName.Value
+    $ServiceDeploymentContext.Variables["StatusFunctionServicePrincipalId"] = $DeploymentResult.Outputs.controlFunctionServicePrincipalId.Value
+    $ServiceDeploymentContext.Variables["ControlFunctionServicePrincipalId"] = $DeploymentResult.Outputs.statusFunctionServicePrincipalId.Value
+}
