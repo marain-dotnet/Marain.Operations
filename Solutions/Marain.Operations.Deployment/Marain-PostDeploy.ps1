08c100dee3c57e660b109e018ab24eadd3436b2e
Ian Griffiths <Ian.Griffiths@endjin.com>
2020-02-14 10:13:14 +0000

    Fix inverted package uploads

--- Solutions/Marain.Operations.Deployment/Marain-PostDeploy.ps1
+++ Solutions/Marain.Operations.Deployment/Marain-PostDeploy.ps1
@@ -7,34 +7,14 @@ have been deployed.
 # Marain.Instance expects us to define just this one function.
 Function MarainDeployment([MarainServiceDeploymentContext] $ServiceDeploymentContext) {
 
-    Write-Host 'Grant the functions access to the KV'
-
-    #$InstanceResourceGroupName = $InstanceDeploymentContext.MakeResourceGroupName("operations")
-    #$KeyVaultName = $ServiceDeploymentContext.Variables["KeyVaultName"]
-    #$StatusPrincipalId = $ServiceDeploymentContext.Variables["StatusFunctionServicePrincipalId"]
-    #$ControlPrincipalId = $ServiceDeploymentContext.Variables["ControlFunctionServicePrincipalId"]
-
-    # Apparently we're doing this through the template
-    #Set-AzKeyVaultAccessPolicy `
-    #    -VaultName $KeyVaultName `
-    #    -ResourceGroupName $InstanceResourceGroupName `
-    #    -ObjectId $StatusPrincipalId `
-    #    -PermissionsToSecrets Get
-    #
-    #Set-AzKeyVaultAccessPolicy `
-    #    -VaultName $KeyVaultName `
-    #    -ResourceGroupName $InstanceResourceGroupName `
-    #    -ObjectId $ControlPrincipalId `
-    #    -PermissionsToSecrets Get
-
     Write-Host 'Uploading function code packages'
 
     $ServiceDeploymentContext.UploadReleaseAssetAsAppServiceSitePackage(
-        "Marain.Operations.ControlHost.zip",
+        "Marain.Operations.StatusHost.zip",
         $ServiceDeploymentContext.AppName + "status"
     )
     $ServiceDeploymentContext.UploadReleaseAssetAsAppServiceSitePackage(
-        "Marain.Operations.StatusHost.zip",
+        "Marain.Operations.ControlHost.zip",
         $ServiceDeploymentContext.AppName + "control"
     )
 
