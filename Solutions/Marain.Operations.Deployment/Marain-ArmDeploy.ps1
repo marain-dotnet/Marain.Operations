bdb1e6917c553181d10cdeb8c5125d691e68f950
James Dawson <james.dawson@endjin.com>
2020-05-12 22:31:46 +0100

    Updated ARM deployment with service tenant configuration details
    This is currently hard-coded as 'well-known' values, but will need to be more discoverable in future.

--- Solutions/Marain.Operations.Deployment/Marain-ArmDeploy.ps1
+++ Solutions/Marain.Operations.Deployment/Marain-ArmDeploy.ps1
@@ -6,6 +6,10 @@ script. It is our opportunity to create Azure resources.
 # Marain.Instance expects us to define just this one function.
 Function MarainDeployment([MarainServiceDeploymentContext] $ServiceDeploymentContext) {
 
+    # TODO: make this discoverable
+    $serviceTenantId = '3633754ac4c9be44b55bfe791b1780f12429524fe7b6cc48a265a307407ec858'
+    $serviceTenantDisplayName = 'Operations v1'
+
     [MarainAppService]$TenancyService = $ServiceDeploymentContext.InstanceContext.GetCommonAppService("Marain.Tenancy")
 
     $OperationsControlAppId = $ServiceDeploymentContext.GetAppId("control")
@@ -15,6 +19,8 @@ Function MarainDeployment([MarainServiceDeploymentContext] $ServiceDeploymentCon
         tenancyServiceResourceIdForMsiAuthentication=$TenancyService.AuthAppId
         tenancyServiceBaseUri=$TenancyService.BaseUrl
         appInsightsInstrumentationKey=$ServiceDeploymentContext.InstanceContext.ApplicationInsightsInstrumentationKey
+        marainServiceTenantId=$serviceTenantId
+        marainServiceTenantDisplayName=$serviceTenantDisplayName
     }
     $InstanceResourceGroupName = $InstanceDeploymentContext.MakeResourceGroupName("operations")
     $DeploymentResult = $ServiceDeploymentContext.InstanceContext.DeployArmTemplate(
