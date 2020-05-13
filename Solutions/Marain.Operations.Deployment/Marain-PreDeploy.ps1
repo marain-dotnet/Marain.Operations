d2c67c5d07724a5451b393c5e42b738069413d03
James Dawson <james.dawson@endjin.com>
2020-05-13 13:38:53 +0100

    Add more error handling to catch issues when running the marain cli

--- Solutions/Marain.Operations.Deployment/Marain-PreDeploy.ps1
+++ Solutions/Marain.Operations.Deployment/Marain-PreDeploy.ps1
@@ -26,9 +26,14 @@ Function MarainDeployment([MarainServiceDeploymentContext] $ServiceDeploymentCon
 
     # ensure the service tenancy exists
     $serviceManifest = Join-Path $PSScriptRoot "..\ServiceManifests\OperationsServiceManifest.jsonc" -Resolve
-    $cliOutput = & $ServiceDeploymentContext.InstanceContext.MarainCliPath create-service $serviceManifest
-    if ( $LASTEXITCODE -ne 0 -and -not ($cliOutput -imatch 'service tenant.*already exists') ) {
-        # TODO: Ignore error when service tenant already exists
-        Write-Error "Error whilst trying to register the Operations service tenant: ExitCode=$LASTEXITCODE`n$cliOutput"
+    try {
+        $cliOutput = & $ServiceDeploymentContext.InstanceContext.MarainCliPath create-service $serviceManifest
+        if ( $LASTEXITCODE -ne 0 -and -not ($cliOutput -imatch 'service tenant.*already exists') ) {
+            # TODO: Ignore error when service tenant already exists
+            Write-Error "Error whilst trying to register the Operations service tenant: ExitCode=$LASTEXITCODE`n$cliOutput"
+        }
+    }
+    catch {
+        throw $_
     }
 }
