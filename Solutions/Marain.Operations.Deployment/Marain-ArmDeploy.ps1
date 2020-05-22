9dde10c2cba4a24188de586db354d5f55ab82b86
James Dawson <james.dawson@endjin.com>
2020-05-22 07:42:19 +0100

    Updated powershell deployment (#113)
    Adds support for configuring the Operations service tenancy and relies on the main deployment in Marain.Instance for ensuring the `marain` .net global tool is available.
    
    Co-authored-by: Jonathan George <jon@jonathangeorge.co.uk>

--- Solutions/Marain.Operations.Deployment/Marain-ArmDeploy.ps1
+++ Solutions/Marain.Operations.Deployment/Marain-ArmDeploy.ps1
@@ -6,6 +6,10 @@ script. It is our opportunity to create Azure resources.
 # Marain.Instance expects us to define just this one function.
 Function MarainDeployment([MarainServiceDeploymentContext] $ServiceDeploymentContext) {
 
+    # TODO: make this discoverable
+    $serviceTenantId = '3633754ac4c9be44b55bfe791b1780f12429524fe7b6cc48a265a307407ec858'
+    $serviceTenantDisplayName = 'Operations v1'
+
     [MarainAppService]$TenancyService = $ServiceDeploymentContext.InstanceContext.GetCommonAppService("Marain.Tenancy")
 
     $OperationsControlAppId = $ServiceDeploymentContext.GetAppId("control")
@@ -15,6 +19,8 @@ Function MarainDeployment([MarainServiceDeploymentContext] $ServiceDeploymentCon
         tenancyServiceResourceIdForMsiAuthentication=$TenancyService.AuthAppId
         tenancyServiceBaseUri=$TenancyService.BaseUrl
         appInsightsInstrumentationKey=$ServiceDeploymentContext.InstanceContext.ApplicationInsightsInstrumentationKey
+        marainServiceTenantId=$serviceTenantId
+        marainServiceTenantDisplayName=$serviceTenantDisplayName
     }
     $InstanceResourceGroupName = $InstanceDeploymentContext.MakeResourceGroupName("operations")
     $DeploymentResult = $ServiceDeploymentContext.InstanceContext.DeployArmTemplate(
@@ -25,4 +31,18 @@ Function MarainDeployment([MarainServiceDeploymentContext] $ServiceDeploymentCon
 
     $ServiceDeploymentContext.SetAppServiceDetails($DeploymentResult.Outputs.controlFunctionServicePrincipalId.Value, "status", $null)
     $ServiceDeploymentContext.SetAppServiceDetails($DeploymentResult.Outputs.statusFunctionServicePrincipalId.Value, "control", $null)
+
+
+    # ensure the service tenancy exists
+    Write-Host "Ensuring Operations service tenancy..."
+    $serviceManifest = Join-Path $PSScriptRoot "ServiceManifests\OperationsServiceManifest.jsonc" -Resolve
+    try {
+        $cliOutput = & $ServiceDeploymentContext.InstanceContext.MarainCliPath create-service $serviceManifest
+        if ( $LASTEXITCODE -ne 0 -and -not ($cliOutput -imatch 'service tenant.*already exists') ) {
+            Write-Error "Error whilst trying to register the Operations service tenant: ExitCode=$LASTEXITCODE`n$cliOutput"
+        }
+    }
+    catch {
+        throw $_
+    }
 }
